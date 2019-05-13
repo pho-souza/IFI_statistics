3bc349931b00a8e187d2f897de90caf93c3f3cc6
pedroka196 <pedroka196@gmail.com>
2019-05-13 14:55:12 -0300

    Update
    Adicionado diversos elementos e update de execução.

---
+++ codigos.R
@@ -0,0 +1,19 @@
+
+### RAFs
+#####################################################################################################
+
+#Codigos_RAF <- c(	529484 , 529485 , 529486 , 529613 , 529913 , 530364 , 530905 , 531520 , 532983 , 534520 , 535500 , 536464 ,
+#			538087 , 539124 , 540164 , 541284 , 542807 , 543844 , 545483)
+
+#Numero_dos_RAF <- 1:19
+
+#Meses_RAF <- c(			     "Fevereiro 2017" , "Mar?o 2017" , "Abril 2017" , "Maio 2017" , "Junho 2017" , "Julho 2017" , "Agosto 2017" , "Setembro 2017" , "Outubro 2017" , "Novembro 2017" , "Dezembro 2017" ,
+#			"Janeiro 2018" , "Fevereiro 2018" , "Mar?o 2018" , "Abril 2018" , "Maio 2018" , "Junho 2018" , "Julho 2018" , "Agosto 2018")
+
+#RAF <- matrix(c(Numero_dos_RAF,Codigos_RAF),ncol=2)
+#dimnames(RAF) <- list(Meses_RAF,c("N?mero","C?digo"))
+
+#save(RAF,file="RAF")
+
+
+#################### Notas Tecnicas
