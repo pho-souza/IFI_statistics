8901984c74670f10583e091e35b32a01b909d8a5
pedroka196 <pedroka196@gmail.com>
2019-05-14 17:34:33 -0300

    Add files via upload

---
+++ runapp.R
@@ -0,0 +1,11 @@
+library(shiny)
+
+setwd("U://Pastas pessoais/Pedro/Códigos e experimentos/R/acessos/")
+
+source("aplicativo/pacotes.R")
+
+load(file = "aplicativo/base01")
+load(file = "aplicativo/base02")
+load(file = "aplicativo/base03")
+
+runApp(appDir = "aplicativo/")
